09c10eeff75792acdccb40dc82691bda5d333220
Artem Zubkov <oldschool1705@gmail.com>
2023-08-23 15:38:25 +0300

    Add real-time listening

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,49 @@
 <script setup lang="ts">
-import { onMounted, reactive } from 'vue'
-import { collection, query, where, getDocs } from 'firebase/firestore'
+import { onMounted, ref } from 'vue'
+import { collection, query, getDocs, onSnapshot } from 'firebase/firestore'
 import { db } from './firebase/config'
 
 interface Song {
-  id: string;
-  title: string;
-  artist: string;
-  year: string;
+  id: string
+  title: string
+  artist: string
+  year: string
 }
 
-const songs: Song[] = reactive([])
+const songs = ref<Song[]>([])
 
 onMounted(async () => {
   const q = query(collection(db, 'songs'))
-
-  const querySnapshot = await getDocs(q)
-  querySnapshot.forEach((doc) => {
-    console.log(doc);
-    
-    songs.push({
-      id: doc.id,
-      ...doc.data(),
-    } as Song)
+  onSnapshot(q, (querySnapshot) => {
+    const tempSong: Song[] = []
+    querySnapshot.forEach((doc) => {
+      tempSong.push({id: doc.id, ...doc.data(), } as Song)
+    })
+    songs.value = tempSong
   })
 })
 </script>
 
 <template>
-    <main>
-      <v-card class="mx-auto pa-2" max-width="300">
-        <v-list>
-          <v-list-subheader>Favorite songs</v-list-subheader>
+  <main>
+    <v-card class="mx-auto pa-2" max-width="300">
+      <v-list>
+        <v-list-subheader>Favorite songs</v-list-subheader>
 
-          <v-list-item
-            v-for="(song, i) in songs"
-            :key="i"
-            :value="song"
-            color="primary"
-            rounded="shaped"
-          >
-            <template v-slot:prepend>
-              <v-icon icon="mdi-headphones"></v-icon>
-            </template>
+        <v-list-item
+          v-for="(song, i) in songs"
+          :key="i"
+          :value="song"
+          color="primary"
+          rounded="shaped"
+        >
+          <template v-slot:prepend>
+            <v-icon icon="mdi-headphones"></v-icon>
+          </template>
 
-            <v-list-item-title v-text="song.title"></v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </main>
+          <v-list-item-title v-text="song.title"></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </main>
 </template>
