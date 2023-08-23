91bd314403f8cd5bca3a63e6c079e726e1d7f9e0
Artem Zubkov <oldschool1705@gmail.com>
2023-08-23 15:22:57 +0300

    use v-list and install icons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,52 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, reactive } from 'vue'
 import { collection, query, where, getDocs } from 'firebase/firestore'
 import { db } from './firebase/config'
+
+interface Song {
+  id: string;
+  title: string;
+  artist: string;
+  year: string;
+}
+
+const songs: Song[] = reactive([])
 
 onMounted(async () => {
   const q = query(collection(db, 'songs'))
 
   const querySnapshot = await getDocs(q)
   querySnapshot.forEach((doc) => {
-    console.log(doc.id, ' => ', doc.data())
+    console.log(doc);
+    
+    songs.push({
+      id: doc.id,
+      ...doc.data(),
+    } as Song)
   })
 })
 </script>
 
-<template></template>
+<template>
+    <main>
+      <v-card class="mx-auto pa-2" max-width="300">
+        <v-list>
+          <v-list-subheader>Favorite songs</v-list-subheader>
+
+          <v-list-item
+            v-for="(song, i) in songs"
+            :key="i"
+            :value="song"
+            color="primary"
+            rounded="shaped"
+          >
+            <template v-slot:prepend>
+              <v-icon icon="mdi-headphones"></v-icon>
+            </template>
+
+            <v-list-item-title v-text="song.title"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </main>
+</template>
