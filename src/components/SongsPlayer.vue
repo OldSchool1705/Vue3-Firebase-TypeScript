93ed7994dbe06408bc6e2b00badd0db49f54b47b
Artem Zubkov <oldschool1705@gmail.com>
2023-08-24 10:41:19 +0300

    SongsPlayer component

---
+++ src/components/SongsPlayer.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <v-card title="Player" class="player">
+    <v-card-text>
+      <v-icon icon="mdi-play"></v-icon>
+      SOng title
+    </v-card-text>
+    <v-card-actions>
+      <v-btn>Add to favorites</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<style scoped>
+.player {
+    margin-bottom: 20px;
+}
+</style>
